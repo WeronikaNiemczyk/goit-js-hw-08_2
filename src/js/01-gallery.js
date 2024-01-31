49aa6fa6927dfa4d161904f44eb08dbd3f6a7d46
Weronika Niemczyk <weronika.tlusciak@gmail.com>
2024-01-31 18:41:49 +0100

    task 1 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 console.log(galleryItems);
+
+const galleryPlace = document.querySelector('.gallery');
+
+const gallery = galleryItems
+  .map(
+    item =>
+      `<div class="gallery">
+      <a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>
+    </div>`
+  )
+  .join('');
+
+galleryPlace.innerHTML = gallery;
+
+new SimpleLightbox('.gallery a', {
+  captions: true,
+
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
